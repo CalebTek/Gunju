b10fe2fc7578bd79ee061e32961e8cdd92c7c87c
CalebTek <olawale.odeyemi@decagon.dev>
2024-06-06 11:38:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function applyThemeBasedOnTime() {
   const hour = new Date().getHours();
   if (hour >= 6 && hour < 18) {
-    document.body.classList.remove("light-theme");
-    updateButtonClass("btn-outline-light", "btn-outline-dark");
-  } else {
     document.body.classList.add("light-theme");
     updateButtonClass("btn-outline-dark", "btn-outline-light");
+  } else {
+    document.body.classList.remove("light-theme");
+    updateButtonClass("btn-outline-light", "btn-outline-dark");
   }
 }
 
